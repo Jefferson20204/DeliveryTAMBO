234d9fadcf2dbca132c205a719b215bbb3da383a
SherooRV <u22312945@utp.edu.pe>
2025-06-17 11:41:45 -0500

    Resuelvo conflicto en configApi.js

--- Fronted/src/api/configApi.js
+++ Fronted/src/api/configApi.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-// src/api/configApi.js
-import axios from "axios";
-import { getHeaders } from "./constant";
-
-const BASE_URL = "http://localhost:9090/api/admin/config";
-
-export const getProductSectionsConfig = async () => {
-  const { data } = await axios.get(`${BASE_URL}/product-sections`, {
-    headers: getHeaders(),
-  });
-  return data; // [{ id, categoryId, maxProducts, position }]
-=======
 import axios from "axios";
 import { API_BASE_URL, getHeaders } from "./constant";
 
@@ -22,17 +9,12 @@ export const getProductSectionsConfig = async () => {
     }
   );
   return data;
->>>>>>> e87fda2524a0265c9281c2166a4703b61369ad60
 };
 
 export const createProductSection = async (section) => {
   try {
     const response = await axios.post(
-<<<<<<< HEAD
-      `${BASE_URL}/product-sections/new`,
-=======
       `${API_BASE_URL}/api/admin/config/product-sections/new`,
->>>>>>> e87fda2524a0265c9281c2166a4703b61369ad60
       section,
       {
         headers: getHeaders(),
@@ -48,11 +30,7 @@ export const createProductSection = async (section) => {
 export const updateProductSection = async (id, section) => {
   try {
     const response = await axios.put(
-<<<<<<< HEAD
-      `${BASE_URL}/product-sections/update/${id}`,
-=======
       `${API_BASE_URL}/api/admin/config/product-sections/update/${id}`,
->>>>>>> e87fda2524a0265c9281c2166a4703b61369ad60
       section,
       { headers: getHeaders() }
     );
@@ -64,16 +42,10 @@ export const updateProductSection = async (id, section) => {
 };
 
 export const deleteProductSection = async (id) => {
-<<<<<<< HEAD
-  await axios.delete(`${BASE_URL}/product-sections/delete/${id}`, {
-    headers: getHeaders(),
-  });
-=======
   await axios.delete(
     `${API_BASE_URL}/api/admin/config/product-sections/delete/${id}`,
     {
       headers: getHeaders(),
     }
   );
->>>>>>> e87fda2524a0265c9281c2166a4703b61369ad60
 };
